32424bb65e17dd41ab35f42a487e5d052a8692c1
irvin pineda <62679993+irvinpinedaUCA@users.noreply.github.com>
2021-10-25 15:43:37 -0600

    :sparkles: layout done

---
+++ build/js/Product.js
@@ -0,0 +1,12 @@
+import "tailwindcss/tailwind.css";
+class Computer{
+    computer(marca, modelo, ram, almacenamiento, procesador, color, imagen){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ram = ram;
+        this.almacenamiento = almacenamiento;
+        this.procesador = procesador;
+        this.color = color;
+        this.imagen = imagen;
+    }
+}
