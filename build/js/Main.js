483b72b73c66d3c1e44ba5521f122567ff3de9ed
Josuepp <62664664+Josuepp@users.noreply.github.com>
2021-10-25 21:44:12 -0600

    Add: FormData and ShowData

---
+++ build/js/Main.js
@@ -0,0 +1,49 @@
+
+let computerList = [];
+let form = document.getElementById('formProduct')
+
+
+form.addEventListener('submit', (e) => {
+
+    let formData = new FormData(form);
+    let computer = new Computer;
+    e.preventDefault();
+    computer.computer(formData.get('marca'),formData.get('modelo'),formData.get('color')
+    ,formData.get('procesador'),formData.get('cpuType'),formData.get('ram'),formData.get('almacenamiento')
+    ,formData.get('url'));
+
+    computerList.unshift(computer);
+    localStorage.setItem('item',JSON.stringify(computerList));
+
+    var items = localStorage.getItem('item');
+    var datos = JSON.parse(items);
+
+    let mostrar = document.querySelector(".show")
+    mostrar.innerHTML = "";
+    let show = datos.map((item) => {
+    mostrar.innerHTML += 
+    `
+    <img src ="${item.imagen}">
+    <p class="mb-2">${item.marca}</p>
+    <p class="mb-2">${item.modelo}</p>
+    <p class="mb-2">${item.ram}</p>
+    <p class="mb-2">${item.almacenamiento}</p>
+    <p class="mb-2">${item.procesador}</p>
+    <p class="mb-2">${item.tipoProcesador}</p>
+    `
+});
+
+   
+})
+
+console.log(computerList)
+
+
+
+
+
+
+
+
+
+
--- build/js/Product.js
+++ build/js/Product.js
@@ -1,11 +1,13 @@
-import "tailwindcss/tailwind.css";
-class Computer{
-    computer(marca, modelo, ram, almacenamiento, procesador, color, imagen){
+
+
+class Computer {
+    computer(marca, modelo, ram, almacenamiento, procesador,tipoProcesador, color, imagen){
         this.marca = marca;
         this.modelo = modelo;
         this.ram = ram;
         this.almacenamiento = almacenamiento;
         this.procesador = procesador;
+        this.tipoProcesador = tipoProcesador;
         this.color = color;
         this.imagen = imagen;
     }
