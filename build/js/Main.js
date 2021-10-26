028a02f1c6b70200f1648216b4b0852b69aded9e
irvin pineda <62679993+irvinpinedaUCA@users.noreply.github.com>
2021-10-26 00:32:08 -0600

    :sparkles: binding variables:done,added design

--- build/js/Main.js
+++ build/js/Main.js
@@ -15,13 +15,17 @@ function showInfo(){
     let show = datos.map((item) => {
     mostrar.innerHTML += 
         `
-        <img src ="${item.imagen}">
-        <p class="mb-2">${item.marca}</p>
-        <p class="mb-2">${item.modelo}</p>
-        <p class="mb-2">${item.ram}</p>
-        <p class="mb-2">${item.almacenamiento}</p>
-        <p class="mb-2">${item.procesador}</p>
-        <p class="mb-2">${item.tipoProcesador}</p>
+        <div class="sm:block w-full md:flex justify-center items-center mb-4 border-b-2 border-black">
+        <div class=""><img class="w-4/5 inline-block" src ="${item.imagen}"></div>
+        <div class="w-full">
+        <p class="mb-2 ">-Marca: ${item.marca}</p>
+        <p class="mb-2">-Modelo: ${item.modelo}</p>
+        <p class="mb-2">-Color: ${item.color}</p>
+        <p class="mb-2">-Memoria Ram:${item.ram}</p>
+        <p class="mb-2">-Almacenamiento: ${item.almacenamiento}</p>
+        <p class="mb-2">-Procesador: ${item.tipoProcesador} ${item.procesador}</p>
+        </div>
+        </div>
         `
     });
 }
@@ -56,7 +60,7 @@ form.addEventListener('submit', (e) => {
     `
 }); */
 
-   showInfo();
+showInfo();
 })
 showInfo();
 console.log(computerList)
--- build/js/Product.js
+++ build/js/Product.js
@@ -1,12 +1,12 @@
 class Computer {
-    computer(marca, modelo, ram, almacenamiento, procesador,tipoProcesador, color, imagen){
+    computer(marca, modelo, color, procesador,tipoProcesador, ram, almacenamiento, imagen){
         this.marca = marca;
         this.modelo = modelo;
-        this.ram = ram;
-        this.almacenamiento = almacenamiento;
+        this.color = color;
         this.procesador = procesador;
         this.tipoProcesador = tipoProcesador;
-        this.color = color;
+        this.ram = ram;
+        this.almacenamiento = almacenamiento;
         this.imagen = imagen;
     }
 }
